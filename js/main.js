92148e773deaa0cb806cf02d1334d7fc3ef1ef37
quakush <quakush@mail.ru>
2021-03-30 14:08:39 +0700

    move

--- js/main.js
+++ js/main.js
@@ -7,6 +7,21 @@ var PHOTOS = ['hotel1', 'hotel2', 'hotel3'];
 
 var map = document.querySelector('.map');
 var mapPins = map.querySelector('.map__pins');
+var mapPinMain = map.querySelector('.map__pin--main');
+var adForm = document.querySelector('.ad-form');
+var adFormInput = adForm.querySelectorAll('input');
+var adFormSelect = adForm.querySelectorAll('select');
+var adFormTextArea = adForm.querySelectorAll('textarea');
+var adFormButton = adForm.querySelectorAll('button');
+var adForm = document.querySelector('.ad-form');
+var typeRoom = adForm.querySelector('#type');
+var priceForm = adForm.querySelector('#price');
+var timeIn = adForm.querySelector('#timein');
+var timeOut = adForm.querySelector('#timeout');
+var adFormAddress = adForm.querySelector('#address');
+
+var MAP_WIDTH = mapPins.clientWidth;
+var MAP_HEIGHT = mapPins.clientHeight;
 
 var getRandom = function(x) {
   var num = Math.floor(Math.random(x) * x);
@@ -151,11 +166,165 @@ var renderCard = function(pins) {
   card.querySelector('.popup__text--time').textContent = 'Заезд после ' + pin.offer.checkin + ', выезд после ' + pin.offer.checkout;
   card.querySelector('.popup__description').textContent = pin.offer.description;
   card.querySelector('.popup__avatar').src = pin.author.avatar;
+  console.log(pin.offer.address);
+  adForm.querySelector('#address').value = pin.offer.address;
 
   mapPins.insertAdjacentElement('afterend', card);
 };
 
-var pins = makePins();
-map.classList.remove('map--faded');
-drawPins(pins);
-renderCard(pins);
+var disabledForm = function() {
+  adFormInput.forEach(el => {
+    el.setAttribute('disabled', true);
+  });
+
+  adFormSelect.forEach(el => {
+    el.setAttribute('disabled', true);
+  });
+
+  adFormTextArea.forEach(el => {
+    el.setAttribute('disabled', true);
+  });
+
+  adFormButton.forEach(el => {
+    el.setAttribute('disabled', true);
+  });
+};
+
+var enabledForm = function() {
+  adFormInput.forEach(el => {
+    el.removeAttribute('disabled');
+  });
+
+  adFormSelect.forEach(el => {
+    el.removeAttribute('disabled');
+  });
+
+  adFormTextArea.forEach(el => {
+    el.removeAttribute('disabled');
+  });
+
+  adFormButton.forEach(el => {
+    el.removeAttribute('disabled');
+  });
+
+  adForm.classList.remove('ad-form--disabled');
+};
+
+var onRemoveFaded = function() {
+  var pins = makePins();
+
+  map.classList.remove('map--faded');
+  drawPins(pins);
+  renderCard(pins);
+  enabledForm();
+};
+
+var onChangeType = function() {
+  switch (typeRoom.value) {
+    case 'bungalo':
+      priceForm.placeholder = '0';
+      priceForm.setAttribute('min', '0');
+      break;
+    case 'flat':
+      priceForm.placeholder = '1000';
+      priceForm.setAttribute('min', '1000');
+      break;
+    case 'house':
+      priceForm.placeholder = '5000';
+      priceForm.setAttribute('min', '5000');
+      break;
+    case 'palace':
+      priceForm.placeholder = '10000';
+      priceForm.setAttribute('min', '10000');
+      break;
+  }
+};
+
+var onChangeTimeIn = function() {
+  switch (timeIn.value) {
+    case '12:00':
+      timeOut.value = '12:00';
+      break;
+    case '13:00':
+      timeOut.value = '13:00';
+      break;
+    case '14:00':
+      timeOut.value = '14:00';
+      break;
+  }
+};
+
+var onChangeTimeOut = function() {
+  switch (timeOut.value) {
+    case '12:00':
+      timeIn.value = '12:00';
+      break;
+    case '13:00':
+      timeIn.value = '13:00';
+      break;
+    case '14:00':
+      timeIn.value = '14:00';
+      break;
+  }
+};
+
+disabledForm();
+mapPinMain.addEventListener('mousedown', onRemoveFaded);
+typeRoom.addEventListener('change', onChangeType);
+timeIn.addEventListener('change', onChangeTimeIn);
+timeOut.addEventListener('change', onChangeTimeOut);
+
+mapPinMain.addEventListener('mousedown', function (evt) {
+  mapPinMain.removeEventListener('mousedown', onRemoveFaded);
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    adFormAddress.value = mapPinMain.offsetLeft + Math.round(mapPinMain.clientWidth/2) + ', ' + (mapPinMain.offsetTop + mapPinMain.clientHeight)
+    mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
+    mapPinMain.style.left = (mapPinMain.offsetLeft - shift.x) + 'px';
+
+    if (mapPinMain.offsetTop <= 0) {
+      mapPinMain.style.top = 0 + 'px';
+    }
+
+    if (mapPinMain.offsetLeft <= 0) {
+      mapPinMain.style.left = 0 + 'px';
+    }
+
+    if (mapPinMain.offsetTop >= (MAP_HEIGHT - mapPinMain.clientHeight)) {
+      mapPinMain.style.top = MAP_HEIGHT - mapPinMain.clientHeight + 'px';
+    }
+
+    if (mapPinMain.offsetLeft >= (MAP_WIDTH - mapPinMain.clientWidth)) {
+      mapPinMain.style.left = MAP_WIDTH - mapPinMain.clientWidth + 'px';
+    }
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
+
+
